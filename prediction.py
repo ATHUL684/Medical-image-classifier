c73c27b8d0a130f44e288904ac3cdc644213836e
ATHUL684 <athulkrishna138@gmail.com>
2021-08-08 20:33:40 +0530

    Initial commit

---
+++ prediction.py
@@ -0,0 +1,16 @@
+"""
+@author: Athul Krishna VS
+"""
+import cv2
+import tensorflow as tf
+CATEGORIES = ["effusion","infiltration","normal","nodule","pneumothorax"]
+def prepare(file):
+    IMG_SIZE = 50
+    img_array = cv2.imread(file, cv2.IMREAD_GRAYSCALE)
+    new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
+    return new_array.reshape(-1, IMG_SIZE, IMG_SIZE, 1)
+model = tf.keras.models.load_model("CNN.model")
+image = prepare("eff.png") #your image path 
+prediction = model.predict([image])
+prediction = list(prediction[0])
+print(CATEGORIES[prediction.index(max(prediction))])
